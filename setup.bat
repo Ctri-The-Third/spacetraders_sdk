c4c29ca4682e159be749b692291d0d364bf00e39
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-09-07 23:32:43 +0100

    v1.0 release

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 py -m build 
 py -m pip uninstall straders_sdk -y
-py -m pip install dist/straders-0.8.0-py3-none-any.whl --force-reinstall
+py -m pip install dist/straders-1.0.0-py3-none-any.whl --force-reinstall
 explorer "C:\Users\C_tri\AppData\Local\Programs\Python\Python310\Lib\site-packages"
