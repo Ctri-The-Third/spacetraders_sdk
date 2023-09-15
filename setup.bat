8462110f779ccd868563db7a39bd0adefff234ae
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-09-15 11:27:31 +0100

    chagne version to 1.1.1

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 py -m build 
 py -m pip uninstall straders_sdk -y
-py -m pip install dist/straders-1.1.0-py3-none-any.whl --force-reinstall
+py -m pip install dist/straders-1.1.1-py3-none-any.whl --force-reinstall
 explorer "C:\Users\C_tri\AppData\Local\Programs\Python\Python310\Lib\site-packages"
