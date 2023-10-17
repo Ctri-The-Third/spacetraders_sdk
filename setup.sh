7f6631dafb2b37e450885ff26e248892722b8a59
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-10-17 10:34:19 +0100

    set to version 1.2.2

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 python3 -m build
-python3 -m pip uninstall straders_sdk -y
-python3 -m pip install dist/straders-1.2.1-py3-none-any.whl --force-reinstall
+python3 -m pip uninstall straders -y
+python3 -m pip install dist/straders-1.2.2-py3-none-any.whl --force-reinstall
