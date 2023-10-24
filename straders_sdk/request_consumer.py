7971b9db5fdb371527d256622d02dd030d0cd36f
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-10-24 18:45:54 +0100

    create prototype consumer

---
+++ straders_sdk/request_consumer.py
@@ -0,0 +1,68 @@
+from datetime import datetime, timedelta
+from threading import Event, Thread
+from requests import PreparedRequest
+from time import sleep
+import requests
+from queue import PriorityQueue
+
+# this consumer should be a singleton.
+# it will have a priority queue of special request objects
+# request objects have a priority, a time of addition to the queue, a request object, and an event.
+
+instance = None
+
+
+class RequestConsumer:
+    _instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def __init__(self, auto_start=True) -> None:
+        if hasattr(self, "stop_flag"):
+            return
+        self.queue = PriorityQueue()
+        self.stop_flag = False
+        self._consumer_thread = Thread(target=self._consume_until_stopped)
+        self._session = requests.Session()
+        if auto_start:
+            self.start()
+        pass
+
+    def stop(self):
+        self.stop_flag = True
+
+    def start(self):
+        self.stop_flag = False
+        self._consumer_thread.start()
+
+    def _consume_until_stopped(self):
+        """this method should be tied to the _consumer_thread"""
+        interval = timedelta(milliseconds=60000 / 165)
+        while not self.stop_flag:
+            if not self.queue.empty():
+                tupe = self.queue.get()
+                package = tupe[1]
+                package: PackageedRequest
+                try:
+                    print("Doing the thing")
+                    package.response = self._session.send(package.request)
+
+                except Exception as e:
+                    print(e)
+                package.event.set()
+            sleep(interval.total_seconds())
+
+    def validate(self, response: requests.Response):
+        pass
+
+
+class PackageedRequest:
+    def __init__(self, priority: float, request: PreparedRequest, event: Event):
+        self.priority = priority
+        self.request = request
+        self.response = None
+        self.event = event
+        self.time_added = datetime.now()
---
+++ tests/test_consumer.py
@@ -0,0 +1,44 @@
+import requests
+from threading import Event
+import straders_sdk.request_consumer as rc
+import pytest
+
+
+def test_u_singleton():
+    """test that the singleton is working"""
+    assert rc.RequestConsumer() is rc.RequestConsumer()
+
+
+def test_u_queue():
+    """test that the queue is working"""
+    consumer = rc.RequestConsumer(auto_start=False)
+
+    consumer.queue.put(rc.PackageedRequest(0, None, None))
+    assert not rc.RequestConsumer().queue.empty()
+
+
+@pytest.mark.execution_timeout(5)
+def test_send_some_requets():
+    """test that the queue is working"""
+    consumer = rc.RequestConsumer(auto_start=False)
+    request = requests.Request("GET", "https://api.spacetraders.io/v2/")
+    prepared_request = request.prepare()
+    request_1 = rc.PackageedRequest(2, prepared_request, Event())
+    request_2 = rc.PackageedRequest(1, prepared_request, Event())
+    request_3 = rc.PackageedRequest(0, prepared_request, Event())
+
+    consumer.queue.put((request_1.priority, request_1))
+    consumer.queue.put((request_2.priority, request_2))
+    consumer.queue.put((request_3.priority, request_3))
+
+    responses = []
+    consumer.start()
+    while len(responses) < 3:
+        if request_1.event.is_set() and 1 not in responses:
+            responses.append(1)
+        if request_2.event.is_set() and 2 not in responses:
+            responses.append(2)
+        if request_3.event.is_set() and 3 not in responses:
+            responses.append(3)
+    consumer.stop()
+    assert responses == [3, 2, 1]
