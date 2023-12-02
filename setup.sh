39382bdfc1a6cf76bd77210872f439393bb45381
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-12-02 19:41:25 +0000

    update build to 2.1.4

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 python3 -m build
 python3 -m pip uninstall straders -y
-python3 -m pip install dist/straders-2.1.2-py3-none-any.whl --force-reinstall
+python3 -m pip install dist/straders-2.1.4-py3-none-any.whl --force-reinstall
