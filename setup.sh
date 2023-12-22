c83718d4384ebe29dfd0eb20997de3b5170f16a3
C'tri Goudie <CtriCoding@Ctri.co.uk>
2023-12-22 21:59:54 +0000

    Revert "Update setup.sh"
    This reverts commit c5787b69a3f12f136f23a6704f56980934fe2940.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,1 @@
-#! /bin/bash
-
-python3 -m build
-python3 -m pip uninstall straders -y
-python3 -m pip install dist/straders-2.1.4-py3-none-any.whl --force-reinstall
+#! /bin/bashpython3 -m buildpython3 -m pip uninstall straders -ypython3 -m pip install dist/straders-2.1.4-py3-none-any.whl --force-reinstall
